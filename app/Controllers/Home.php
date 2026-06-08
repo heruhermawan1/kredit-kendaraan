1986dc1e764843fa4b6b7e8da12d85b3dc3b5eb1
gregoriusbrian <gregorius.bio@gmail.com>
2026-06-08 15:49:44 +0700

    brian mengedit Home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,20 @@
 
 namespace App\Controllers;
 
+use App\Models\VehicleModel;
+
 class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        helper(['form', 'url']);
+
+        $vehicleModel = new VehicleModel();
+        $Vehicles = $vehicleModel->orderBy('dibuat_pada', 'DESC')->findA11(4);
+
+        return view('home',[
+            'vechiles' => $vehicles,
+            'user' => session()->get('user'),
+        ]);
     }
 }
